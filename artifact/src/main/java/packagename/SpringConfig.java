1cdd2748248596a66c93d4885b5d9f45d002b532
fromhans <zihs0822@gmail.com>
2022-02-05 22:47:15 +0900

    SpringData 인터페이스를 통한 Bean 등록을 통해 구현체 자동 사용

--- artifact/src/main/java/packagename/SpringConfig.java
+++ artifact/src/main/java/packagename/SpringConfig.java
@@ -3,35 +3,43 @@ package packagename;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import packagename.repository.JdbcMemberRepository;
-import packagename.repository.JdbcTemplateMemberRepository;
-import packagename.repository.JpaMemberRepository;
-import packagename.repository.MemberRepository;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+import packagename.domain.Member;
+import packagename.repository.*;
 import packagename.service.MemberService;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 
 @Configuration
 public class SpringConfig {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final MemberRepository memberRepository;
+
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
-    }
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberRepository memberRepository() {
+    public MemberService memberService() {
+
+        return new MemberService(memberRepository);
+    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+
 }
--- artifact/src/main/java/packagename/repository/SpringDataJpaMemberRepository.java
+++ artifact/src/main/java/packagename/repository/SpringDataJpaMemberRepository.java
@@ -1,4 +1,13 @@
 package packagename.repository;
 
-public interface SpringDataJpaMemberRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import packagename.domain.Member;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    //JPQL select m from Member m where m.name = ?
+    @Override
+    Optional<Member> findByName(String name);
 }
