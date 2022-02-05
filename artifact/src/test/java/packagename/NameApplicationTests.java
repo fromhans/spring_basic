9edac350c61292dfe7936a826c6611f01c861dbd
fromhans <zihs0822@gmail.com>
2022-02-05 22:01:41 +0900

    추가: 스프링입문강의 기본코드

---
+++ artifact/src/test/java/packagename/NameApplicationTests.java
@@ -0,0 +1,13 @@
+package packagename;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
